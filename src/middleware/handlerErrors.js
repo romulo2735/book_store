471176ad32c9a39d8022e10d5feafa49f21498b6
Romulo Sousa <romulosousa27@hotmail.com>
2025-07-24 20:07:33 -0300

    refactor: change if to switch case and add new validation to fields required apply https 422

--- src/middleware/handlerErrors.js
+++ src/middleware/handlerErrors.js
@@ -2,13 +2,22 @@ import mongoose from "mongoose";
 
 //eslint-disable-next-line no-unused-vars
 export default function handlerErrors(err, req, res, next) {
-    if (err instanceof mongoose.Error.CastError) {
-        res.status(400).send({
-            message: "One or more fields are invalid",
-        });
-    } else {
-        res.status(500).send({
-            message: "Internal Server Error"
-        })
+    switch (true) {
+        case err instanceof mongoose.Error.CastError:
+            res.status(400).send({
+                message: "One or more fields are invalid",
+            });
+            break;
+        case err instanceof mongoose.Error.ValidationError:
+            res.status(422).send({
+                message: "One or more fields are invalid",
+                errors: Object.values(err.errors).map(err => err.message)
+            });
+            break;
+        default:
+            res.status(500).send({
+                message: "Internal Server Error"
+            });
+            break;
     }
 }
